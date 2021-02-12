d68f37eec71329abb1e42b30e165d13b385d0c96
emicolalc <emicolalc@alum.us.es>
2021-02-12 11:10:44 +0100

    (Configuracion)COnfiguracion heroku
    Configuro para desplegar

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -168,6 +168,19 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+BASEURL="https://egc-1230-emicolalc.herokuapp.com" #LA BARRA
+APIS={
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
+}
+
 try:
     from local_settings import *
 except ImportError:
@@ -183,3 +196,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
